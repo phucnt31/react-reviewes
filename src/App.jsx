481b64c002496b85c91a9e07c3b793c01f550ac3
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-16 18:12:19 +0700

    Setup state and render first review

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,27 @@
+import { useState } from "react";
+import data from "./data";
+import { FaQuoteRight } from "react-icons/fa";
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const { id, image, job, name, text } = data[index];
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <div>
+          <h4 className="author">{name}</h4>
+          <p className="job">{job}</p>
+          <p className="info">{text}</p>
+        </div>
+      </article>
+    </main>
+  );
 };
 export default App;
